de8a2e4502442282416b728922fef87eec506870
t-walk161 <t.walk161@gmail.com>
2023-03-09 11:48:28 +0000

    Created inputName func for start

--- Pages/script.js
+++ Pages/script.js
@@ -1,5 +1,16 @@
 //JS#
-
+const regex = /^\D*$/;
 function inputName(){
-    //Function to pull in name on start screen
+    var name = document.getElementById("enterNameBox").value;
+    if(name == "" || name.length < 3 || name.length > 45 || !regex.test(name)) {
+        alert("Names should not be blank, between 3 and 45 letters, and contain no digits")
+    }
+    else{
+        sessionStorage.setItem("userName", name);
+        console.log("Name Successfully Set To " + sessionStorage.getItem("userName"));
+        document.getElementById("enterName").classList.add("hideMe");
+        document.getElementById("enterName").classList.remove("showMe");
+        document.getElementById("modeSelect").classList.add("ShowMe");
+        document.getElementById("modeSelect").classList.remove("hideMe");
+    }
 }
