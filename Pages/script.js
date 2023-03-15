d99c965c518a556f8ce4e8548b8324f3a0f822f1
t-walk161 <t.walk161@gmail.com>
2023-03-15 15:23:32 +0000

    Completed text for scene 3 and began scene 4

--- Pages/script.js
+++ Pages/script.js
@@ -37,17 +37,17 @@ function startGame(){
     document.getElementById("menuMusic").pause();
 }
 //When using the typeText function, the first element inputted should be the id of the element you want to type in, and the second should be the text you want to type.
-function typeText(elementID, text) {
-    var outputElement = document.getElementById(elementID);
+function typeText(element, text) {
+    element.innerHTML = "";
     var charIndex = 0;
     var intervalId = setInterval(function() {
         var nextChar = text.charAt(charIndex);
-        outputElement.innerHTML += nextChar;
+        element.innerHTML += nextChar;
         charIndex++;
         if (charIndex >= text.length) {
             clearInterval(intervalId);
         }
-    }, 10);
+    }, 15);
 }
 function showScene(num) {
     var sections = document.getElementsByTagName("section");
@@ -56,4 +56,59 @@ function showScene(num) {
     }
     document.getElementById("scene" + num).style.display = "block";
     document.getElementById("backgroundImg").style.backgroundImage = "url('../photos/" + num + ".png')";
+    startScene(num);
+}
+function startScene(num){
+    switch(num){
+        case 2: break;
+        case 3: scene3(); break;
+        case 4: scene4(); break;
+        case 5: break;
+        case 6: break;
+        case 7: break;
+        default: console.log("WARNING: Selected Scene does not have any function");
+    }
+}
+
+//Functions to run scenes
+
+//Scene 3
+function scene3() {
+    var p1 = document.getElementById("textS3P1");
+    typeText(p1 ,"Right, we're inside, it's time to crack on, get out your gun and fire a few shots to scare these people.")
+}
+
+//Scene 4
+function scene4(){
+    var p1 = document.getElementById("textS4P1");
+    typeText(p1 ,"Great that worked better than expected, everyone is on the floor. Do you want to leave someone on crowd control boss, means we can't lift as much from the vault, but also means these civies can't hit the silent alarm.")
+}
+function assignCrowdControl(){
+    var p1 = document.getElementById("textS4P1");
+    typeText(p1 ,"Good call boss, we'll leave someone on crowd control, better safe than sorry. Let's get to work on that vault.")
+    sessionStorage.setItem("remainingTeamMembers", sessionStorage.getItem("remainingTeamMembers") - 1);
+    sessionStorage.setItem("NoOfDecisionsMade", sessionStorage.getItem("NoOfDecisionsMade") + 1);
+    document.getElementById("crowdYes").classList.add("hideMe");
+    document.getElementById("crowdNo").classList.add("hideMe");
+    document.getElementById("crowdContinue").classList.remove("hideMe");
+}
+function notAssignCrowdControl(){
+    var p1 = document.getElementById("textS4P1");
+    let APIurl = "http://andymcdowell.hosting.hal.davecutting.uk/1030_APIs/diceRollWithInputs.php";
+    let args = "?diceFaceNumber=3&diceNumber=1";
+    fetch(APIurl+args)
+    .then(function(response) {
+        if (!response.ok) {
+            throw new Error('Network response was not ok.'); 
+        }
+        return response.json();
+    }).then(function(data) {
+        if (data.err) {
+            alert(data.result);
+        } else {
+            alert(data.result);
+        }
+    }).catch(function(error) {
+        console.log(error.message);
+    });
 }
